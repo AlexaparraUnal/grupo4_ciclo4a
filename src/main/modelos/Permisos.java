77d9643507a978f2fbf996486022a584e10fa65e
MarianaM99 <111700225+MarianaM99@users.noreply.github.com>
2022-11-22 00:18:40 -0500

    Create Permisos.java

---
+++ src/main/modelos/Permisos.java
@@ -0,0 +1,32 @@
+public class Permiso {
+    @Id
+    private String _id;
+    private String url;
+    private String metodo;
+
+    public Permiso(String url, String metodo) {
+        this.url = url;
+        this.metodo = metodo;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
+    }
+}
