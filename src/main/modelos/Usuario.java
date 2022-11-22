34a6410f9e6d34e985e40bb9b40bb868ff3a02d2
MarianaM99 <111700225+MarianaM99@users.noreply.github.com>
2022-11-22 00:15:04 -0500

    Update Usuario.java

--- src/main/modelos/Usuario.java
+++ src/main/modelos/Usuario.java
@@ -1,1 +1,60 @@
 
+public class Usuario {
+   
+    private String _id;
+    private String seudonimo;
+    private String correo;
+    private String contrasena;
+
+    private Rol rol;
+
+    public Usuario(String seudonimo, String correo, String contrasena) {
+        this.seudonimo = seudonimo;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+
+
+    public String getSeudonimo() {
+        return seudonimo;
+    }
+
+    public void setSeudonimo(String seudonimo) {
+        this.seudonimo = seudonimo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
